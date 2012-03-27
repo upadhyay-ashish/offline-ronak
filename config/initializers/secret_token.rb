a0e711aee1dd8476ac6a899b43c6b7ffd3f28a0e
Ashish Upadhyay <ashish_upadhyay@external.mckinsey.com>
2012-03-27 16:01:24 +0530

    Pushing app to git for 1 time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offline::Application.config.secret_token = 'f71d937f77e72d9bebe3f7c8858fb441608e92adb5d27095e80321d0ed7ac005a605d3ce0e9e03ef4fefb503d91119720cd2cf11143c64a6477a8ea30030b7e8'
